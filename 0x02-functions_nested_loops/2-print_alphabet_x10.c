2287b7c90c37d37e527dd74c56aeec3eabf683e1
jesus <jmmacias@uninorte.edu.co>
2022-02-15 15:33:07 -0500

    commit66

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
         int i, j;
-	for(j=1;j<=10;j++){
-        for(i=97;i<123;i++){
-        putchar(i);
-        }
+	for(j = 1;j < 11;j++){
+        	for(i = 97;i < 123;i++){
+        		putchar(i);
+        	}
 	
-        putchar('\n');
+        	putchar('\n');
 	}
 }
 
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
-void main(void){
-	int s;
-
+int main(void){
+	
 	printf("this is the main fuction:");
         /*print_alphabet();*/
-	s = print_last_digit(987);
-	printf("%d", s);
+	print_alphabet_x10();
+	return (0);
+	
 }
