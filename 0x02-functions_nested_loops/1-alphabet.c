258947fd637036c8778bf607d3b1812e93c528f6
Ragnar9902 <84491166+Ragnar9902@users.noreply.github.com>
2022-02-15 15:49:28 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@ void print_alphabet(void)
 {	
 	int i;
 	
-	for(i = 97;i < 123;i++)
+	for (i = 97;i < 123;i++)
 	{
 		
-		putchar(i);
+		putchar (i);
 		
 	}
 	
-	putchar('\n');
+	putchar ('\n');
 }
