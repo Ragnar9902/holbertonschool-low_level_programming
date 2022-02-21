c745d53cc0fc02e3c9321821f7a873aa030c8ec3
Ragnar9902 <84491166+Ragnar9902@users.noreply.github.com>
2022-02-21 16:25:22 -0500

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 - this function take a pointer a variable and update the value to 98
- * @*n : pointer of the variable
+ * @n : pointer of the variable
  */
  void reset_to_98(int *n)
 {
