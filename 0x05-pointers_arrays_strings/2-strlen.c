2acabadb90b439ade8a69b1cb733bf977361afe6
Ragnar9902 <84491166+Ragnar9902@users.noreply.github.com>
2022-02-21 16:47:26 -0500

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - function that returns the length of a string.
  * @s : pointer of string
- * Return : the len of String
+ * Return : the len of String i
  */
 int _strlen(char *s)
 {
