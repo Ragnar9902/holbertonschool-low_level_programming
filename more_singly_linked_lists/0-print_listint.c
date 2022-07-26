db4a0e8da4945230e0b68bb251393bd279ab261b
jesus <jmmacias@uninorte.edu.co>
2022-07-26 18:43:38 -0500

    fit error of betty style

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
 
-/*print_listint - function that print a singly linked list 
+/**
+ *print_listint - function that print a singly linked list
  *const listint_t *h - linked list
  *Return : number of elements of the list
  */
@@ -9,11 +10,11 @@
 size_t print_listint(struct listint_s *h)
 {	int coun = 0;
 	struct listint_s *next = h;
-	
-	while(next != NULL)
+
+	while (next != NULL)
 	{	printf("%i\n", next->n);
 		next = next->next;
 		coun++;
 	}
-	return coun;
+	return (coun);
 }
