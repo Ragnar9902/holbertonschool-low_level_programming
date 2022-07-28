b479448e68d83593fece8daf2f9e0eef602011fa
jesus <jmmacias@uninorte.edu.co>
2022-07-28 16:04:12 -0500

    more singly linked list fit betty-style and doc

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- *print_listint - function that print a singly linked list
- *@h : pointer to node head
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
  *
- *Return: number of elements of the list size_t
+ * Return: number of elements in the list
  */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
 
-size_t print_listint(struct listint_s *h)
-{	int coun = 0;
-	struct listint_s *next = h;
-
-	while (next != NULL)
-	{	printf("%i\n", next->n);
-		next = next->next;
-		coun++;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
-	return (coun);
+	return (count);
 }
